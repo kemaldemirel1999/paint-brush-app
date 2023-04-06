e30ed275fa5f476e5419ed16b326bab169962204
Kemal Demirel <kmldemirel99@gmail.com>
2023-04-06 03:13:22 +0300

    Bugfix draw_pen and dragged items showing first

--- DrawTable.java
+++ DrawTable.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class DrawTable extends JPanel implements MouseMotionListener, MouseListener {
@@ -17,14 +16,15 @@ public class DrawTable extends JPanel implements MouseMotionListener, MouseListe
     //private int first_x, first_y, last_x, last_y;
     private Point start, end;
 
-    private ArrayList<Rectangle> tmp_rectangles = new ArrayList<>();
-    private ArrayList<ArrayList> rectangles = new ArrayList<>();
+    private ArrayList<Object> dragged_rectangle = new ArrayList<>();
+    private ArrayList<Object> dragged_oval = new ArrayList<>();
 
-    private ArrayList<Ellipse2D.Double> tmp_ovals = new ArrayList<>();
-    private ArrayList<ArrayList> ovals = new ArrayList<>();
+    private ArrayList<ArrayList> all_shapes;
 
-    private ArrayList<ArrayList> linePoints = new ArrayList<>();
 
+
+    private ArrayList selectedShape = new ArrayList();
+    private Point lastMousePos = null;
 
     public DrawTable(){
         x = 0;
@@ -34,7 +34,8 @@ public class DrawTable extends JPanel implements MouseMotionListener, MouseListe
         setBackground(Color.GRAY);
         setVisible(true);
         pen_color = new Color(Color.BLACK.getRGB());
-        mode = "pen";
+        mode = "";
+        all_shapes = new ArrayList<>();
     }
 
     @Override
@@ -43,7 +44,6 @@ public class DrawTable extends JPanel implements MouseMotionListener, MouseListe
         old_y = y;
         x = e.getX();
         y = e.getY();
-        System.out.println("x:"+x+", y:"+y);
         Graphics g = getGraphics();
         g.setColor(pen_color);
 
@@ -51,52 +51,111 @@ public class DrawTable extends JPanel implements MouseMotionListener, MouseListe
             ArrayList<Object> tmp = new ArrayList<>();
             tmp.add(new int[] { x, y , old_x, old_y});
             tmp.add(this.pen_color);
-            linePoints.add(tmp);
+            all_shapes.add(tmp);
+
         }
         else if(this.mode.equals("rectangle")){
-            tmp_rectangles.clear();
+            dragged_rectangle.clear();
             end = e.getPoint();
             int x = Math.min(start.x, end.x);
             int y = Math.min(start.y, end.y);
             int width = Math.abs(start.x - end.x);
             int height = Math.abs(start.y - end.y);
             Rectangle rect = new Rectangle(x, y, width, height);
-            tmp_rectangles.add(rect);
+            dragged_rectangle.add(rect);
+            dragged_rectangle.add(this.pen_color);
+
         }
         else if(this.mode.equals("oval")){
-            tmp_ovals.clear();
+            dragged_oval.clear();
             end = e.getPoint();
             int x = Math.min(start.x, end.x);
             int y = Math.min(start.y, end.y);
             int width = Math.abs(start.x - end.x);
             int height = Math.abs(start.y - end.y);
             Ellipse2D.Double oval = new Ellipse2D.Double(x, y, width, height);
-            tmp_ovals.add(oval);
+            dragged_oval.add(oval);
+            dragged_oval.add(this.pen_color);
+        }
+        else if(this.mode.equals("move")){
+            int dx = e.getX() - lastMousePos.x;
+            int dy = e.getY() - lastMousePos.y;
+            System.out.println("Changed: x:"+dx + ", y:"+dy);
+            if(selectedShape.get(0).getClass().equals(Rectangle.class)){
+                Rectangle rect = (Rectangle) selectedShape.get(0);
+                rect.setLocation(rect.x + dx, rect.y + dy);
+            }
+            else if(selectedShape.get(0).getClass().equals(Ellipse2D.Double.class)){
+                Ellipse2D.Double oval = (Ellipse2D.Double) selectedShape.get(0);
+                oval.x = oval.x + dx;
+                oval.y = oval.y + dy;
+            }
+            lastMousePos = e.getPoint();
         }
         repaint();
     }
-    @Override
-    public void mouseMoved(MouseEvent e) {}
-    @Override
-    public void mouseClicked(MouseEvent e) {}
+
     @Override
     public void mousePressed(MouseEvent e) {
         x = e.getX();
         y = e.getY();
         start = e.getPoint();
         System.out.println("start_x:"+start.x+", start_y:"+start.y);
-        if(mode.equals("move")){
+        if(mode.equals("pen")){
             ArrayList<Object> tmp = new ArrayList<>();
-            tmp.add(new int[] { x, y , old_x, old_y});
+            tmp.add(new int[] { x, y , x, y});
             tmp.add(this.pen_color);
-            linePoints.add(tmp);
+            all_shapes.add(tmp);
         }
         else if(mode.equals("oval")){
+            dragged_oval.clear();
+            end = e.getPoint();
+            int x = Math.min(start.x, end.x);
+            int y = Math.min(start.y, end.y);
+            int width = Math.abs(start.x - end.x);
+            int height = Math.abs(start.y - end.y);
+            Ellipse2D.Double oval = new Ellipse2D.Double(x, y, width, height);
+            dragged_oval.add(oval);
+            dragged_oval.add(this.pen_color);
 
         }
         else if(mode.equals("rectangle")){
+            dragged_rectangle.clear();
+            end = e.getPoint();
+            int x = Math.min(start.x, end.x);
+            int y = Math.min(start.y, end.y);
+            int width = Math.abs(start.x - end.x);
+            int height = Math.abs(start.y - end.y);
+            Rectangle rect = new Rectangle(x, y, width, height);
+            dragged_rectangle.add(rect);
+            dragged_rectangle.add(this.pen_color);
 
         }
+        else if(mode.equals("move")){
+            for(ArrayList shape_info: all_shapes){
+                if(shape_info.get(0).getClass().equals(Rectangle.class)){
+                    Rectangle rect = (Rectangle) shape_info.get(0);
+                    if(rect.getBounds().contains(e.getPoint())){
+                        selectedShape.add(rect);
+                        selectedShape.add(shape_info.get(1));
+                        all_shapes.remove(shape_info);
+                        lastMousePos = e.getPoint();
+                        break;
+                    }
+                }
+                else if(shape_info.get(0).getClass().equals(Ellipse2D.Double.class)){
+                    Ellipse2D.Double oval = (Ellipse2D.Double) shape_info.get(0);
+                    if(oval.getBounds().contains(e.getPoint())){
+                        selectedShape.add(oval);
+                        selectedShape.add(shape_info.get(1));
+                        all_shapes.remove(shape_info);
+                        lastMousePos = e.getPoint();
+                        break;
+                    }
+                }
+            }
+        }
+        repaint();
     }
     @Override
     public void mouseReleased(MouseEvent e) {
@@ -107,40 +166,61 @@ public class DrawTable extends JPanel implements MouseMotionListener, MouseListe
         Graphics g = getGraphics();
         g.setColor(this.pen_color);
         if(mode.equals("move")){
-
+            if(selectedShape.get(0).getClass().equals(Rectangle.class)){
+                Rectangle rect = (Rectangle) selectedShape.get(0);
+                ArrayList<Object> tmp = new ArrayList<>();
+                tmp.add(rect);
+                tmp.add(selectedShape.get(1));
+                all_shapes.add(selectedShape);
+            }
+            else if(selectedShape.get(0).getClass().equals(Ellipse2D.Double.class)){
+                Ellipse2D.Double oval = (Ellipse2D.Double) selectedShape.get(0);
+                ArrayList<Object> tmp = new ArrayList<>();
+                tmp.add(oval);
+                tmp.add(selectedShape.get(1));
+                all_shapes.add(selectedShape);
+            }
         }
         else if(mode.equals("oval")){
-            tmp_ovals.clear();
+            dragged_oval.clear();
             int x = Math.min(start.x, end.x);
             int y = Math.min(start.y, end.y);
             int width = Math.abs(start.x - end.x);
             int height = Math.abs(start.y - end.y);
             Ellipse2D.Double oval = new Ellipse2D.Double(x, y, width, height);
-            tmp_ovals.add(oval);
+            dragged_oval.add(oval);
+            dragged_oval.add(this.pen_color);
             ArrayList<Object> info = new ArrayList();
             info.add(oval);
             info.add(this.pen_color);
-            ovals.add(info);
+
+            all_shapes.add(info);
             start = null;
             end = null;
         }
         else if(mode.equals("rectangle")){
-            tmp_rectangles.clear();
+            dragged_rectangle.clear();
             int x = Math.min(start.x, end.x);
             int y = Math.min(start.y, end.y);
             int width = Math.abs(start.x - end.x);
             int height = Math.abs(start.y - end.y);
             Rectangle rect = new Rectangle(x, y, width, height);
-            tmp_rectangles.add(rect);
+            dragged_rectangle.add(rect);
+            dragged_rectangle.add(this.pen_color);
             ArrayList<Object> info = new ArrayList();
             info.add(rect);
             info.add(this.pen_color);
-            rectangles.add(info);
+            all_shapes.add(info);
             start = null;
             end = null;
         }
         repaint();
     }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {}
+    @Override
+    public void mouseClicked(MouseEvent e) {}
     @Override
     public void mouseEntered(MouseEvent e) {}
     @Override
@@ -148,28 +228,35 @@ public class DrawTable extends JPanel implements MouseMotionListener, MouseListe
 
     public void paint(Graphics g){
         super.paint(g);
-        for (ArrayList line_info: linePoints){
-            int[] coordinates = (int[]) line_info.get(0);
-            g.setColor((Color) line_info.get(1));
-            g.drawLine(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+        for(ArrayList shape_info: all_shapes){
+            if(shape_info.get(0).getClass().equals(Rectangle.class)){
+                g.setColor((Color) shape_info.get(1));
+                Rectangle rect = (Rectangle) shape_info.get(0);
+                g.fillRect(rect.x, rect.y, rect.width, rect.height);
+            }
+            else if(shape_info.get(0).getClass().equals(Ellipse2D.Double.class)){
+                g.setColor((Color) shape_info.get(1));
+                Ellipse2D.Double oval = (Ellipse2D.Double) shape_info.get(0);
+                g.fillOval((int) oval.x, (int) oval.y, (int) oval.width, (int) oval.height);
+            }
+            else{
+                System.out.println("yep");
+                int[] coordinates = (int[]) shape_info.get(0);
+                g.setColor((Color) shape_info.get(1));
+                g.drawLine(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+            }
         }
-        for (Rectangle rect : tmp_rectangles) {
-            g.setColor(this.pen_color);
+        if(dragged_rectangle!=null && !dragged_rectangle.isEmpty()){
+            Rectangle rect = (Rectangle) dragged_rectangle.get(0);
+            g.setColor((Color) dragged_rectangle.get(1));
             g.fillRect(rect.x, rect.y, rect.width, rect.height);
+            dragged_rectangle.clear();
         }
-        for (ArrayList rectangle_info : rectangles) {
-            g.setColor((Color) rectangle_info.get(1));
-            Rectangle rect = (Rectangle) rectangle_info.get(0);
-            g.fillRect(rect.x, rect.y, rect.width, rect.height);
-        }
-        for (Ellipse2D.Double oval : tmp_ovals) {
-            g.setColor(this.pen_color);
+        if(dragged_oval != null &&  !dragged_oval.isEmpty()){
+            Ellipse2D.Double oval = (Ellipse2D.Double) dragged_oval.get(0);
+            g.setColor((Color) dragged_oval.get(1));
             g.fillOval((int) oval.x, (int) oval.y, (int) oval.width, (int) oval.height);
-        }
-        for (ArrayList oval_info : ovals) {
-            g.setColor((Color) oval_info.get(1));
-            Ellipse2D.Double oval = (Ellipse2D.Double) oval_info.get(0);
-            g.fillOval((int) oval.x, (int) oval.y, (int) oval.width, (int) oval.height);
+            dragged_oval.clear();
         }
     }
 
